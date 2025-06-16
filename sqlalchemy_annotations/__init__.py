829d3535259c6dd8ae4d40313b0ee404040f2338
Mikhail Shagov <mishaga@users.noreply.github.com>
2025-06-16 16:12:10 +0300

    feat: str enums (#2)
    * feat: tests
    
    * feat: str enums
    
    * feat: str enums
    
    * feat: str enums

--- sqlalchemy_annotations/__init__.py
+++ sqlalchemy_annotations/__init__.py
@@ -1,8 +1,4 @@
-from ._booleans import (
-    BooleanColumn,
-    BooleanDefaultFalseColumn,
-    BooleanDefaultTrueColumn,
-)
+from ._booleans import BooleanColumn, BooleanDefaultFalseColumn, BooleanDefaultTrueColumn
 from ._dates import (
     DateColumn,
     DateTimeColumn,
@@ -11,6 +7,7 @@ from ._dates import (
     TimeColumn,
     TimeWOTimezoneColumn,
 )
+from ._enums import StrEnumColumn
 from ._numbers import (
     BigIntegerColumn,
     BigIntegerIndexColumn,
@@ -23,3 +20,31 @@ from ._numbers import (
 )
 from ._strings import TextColumn, TextIndexColumn, TextPKColumn, TextUniqueColumn
 from ._uuids import UUIDColumn, UUIDIndexColumn, UUIDPKColumn
+
+__all__ = (
+    'BigIntegerColumn',
+    'BigIntegerIndexColumn',
+    'BigIntegerPKColumn',
+    'BigSerialPKColumn',
+    'BooleanColumn',
+    'BooleanDefaultFalseColumn',
+    'BooleanDefaultTrueColumn',
+    'DateColumn',
+    'DateTimeColumn',
+    'DateTimeDefaultUtcNowColumn',
+    'DateTimeWOTimezoneColumn',
+    'IntegerColumn',
+    'IntegerIndexColumn',
+    'IntegerPKColumn',
+    'SerialPKColumn',
+    'StrEnumColumn',
+    'TextColumn',
+    'TextIndexColumn',
+    'TextPKColumn',
+    'TextUniqueColumn',
+    'TimeColumn',
+    'TimeWOTimezoneColumn',
+    'UUIDColumn',
+    'UUIDIndexColumn',
+    'UUIDPKColumn',
+)
